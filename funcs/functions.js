6ad62df12552cf664adc554724aa3eab256b3730
hassaammgl <h455a4mmehtab@gmail.com>
2024-05-23 09:23:24 +0500

    ok

--- funcs/functions.js
+++ funcs/functions.js
@@ -5,8 +5,6 @@ import { createSpinner } from 'nanospinner';
 import { logs } from './utils.js';
 
 const spinner = createSpinner();
-const command = "git clone https://github.com/hassaammgl/client-side-create-my-mern-app.git client && npm i axios react-router-dom react-hot-toast && cd client && npm update @types/react @types/react-dom @vitejs/plugin-react-swc eslint eslint-plugin-react eslint-plugin-react-hooks eslint-plugin-react-refresh vite -S";
-
 const createFolder = async (path) => {
     try {
         await fs.promises.mkdir(path, { recursive: true });
@@ -39,19 +37,19 @@ const initializingApi = async (path, data) => {
     logs.info("initializing Api...");
     if (data.module === "commonjs") {
         logs.info("initializing commonjs api...")
-        await Runner(`cd ${path} && git clone https://github.com/hassaammgl/commonjs-server-create-my-mern-app.git server && cd server && rm -r .git && npm i cors express mongoose dotenv && npm update cors express mongoose dotenv -S`)
+        await Runner(`cd ${path} && git clone https://github.com/hassaammgl/commonjs-server-create-my-mern-app.git server && cd server && rm -r .git && npm i cors express cookie-parser mongoose dotenv && npm update cors express mongoose dotenv -S`)
         logs.success("initialized commonjs api...")
     }
     else {
         logs.info("initializing ESM api...")
-        await Runner(`cd ${path} && git clone https://github.com/hassaammgl/ESM-server-create-my-mern-app.git server && cd server && rm -r .git && npm i cors express mongoose dotenv && npm update cors express mongoose dotenv -S`)
+        await Runner(`cd ${path} && git clone https://github.com/hassaammgl/ESM-server-create-my-mern-app.git server && cd server && rm -r .git && npm i cors express cookie-parser mongoose dotenv && npm update cors express mongoose dotenv -S`)
         logs.success("initialized ESM api...")
     }
 }
 const initializingClient = async (path) => {
     logs.info("initializing Client...");
     logs.info("Updating packages....")
-    await Runner(`cd ${path} && ${command}`);
+    await Runner(`cd ${path} && git clone https://github.com/hassaammgl/client-side-create-my-mern-app.git client && cd client && rm -r .git && npm update @types/react @types/react-dom @vitejs/plugin-react-swc eslint eslint-plugin-react eslint-plugin-react-hooks eslint-plugin-react-refresh vite -S && npm i axios react-router-dom react-hot-toast`);
     logs.success("UPDATED packages....")
 }
 
